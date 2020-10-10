bf38a011a14046d7cfebe04514ca919022dbab7d
Luis Felipe Avila <lfelipeavila@outlook.com>
2020-10-10 19:29:46 -0300

    Alterações no layout
    Inseri uma nova área de banner com o termo forte para o conteúdo da página. Alterei tamanho de textos, posicionamento de elementos link e alterei o texto de apresentação.

---
+++ script/progress-bar.js
@@ -0,0 +1,13 @@
+/* function move() {
+    var elem = document.getElementsByTagName("progress-bar");   
+    var width = 1;
+    var class = setInterval(frame, 10);
+    function frame() {
+      if (width >= 90) {
+        clearInterval(class);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  } */
